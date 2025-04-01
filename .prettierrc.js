790de12db521e3ad69cf03c448989eac2f8f828d
Andrew <dracon.24041230@mail.ru>
2025-04-01 11:28:36 +0300

    start

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+import baseConfig from '@it-incubator/prettier-config';
+
+const config = {
+    ...baseConfig
+};
+
+export default config;
+
+/*   pnpm i @it-incubator/prettier-config   */
+/*   pnpm add --save-dev --save-exact prettier  */
