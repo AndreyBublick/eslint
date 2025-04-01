74ab6b84d9ea416f67be68a9f7284e114ba6b919
Andrew <dracon.24041230@mail.ru>
2025-04-01 12:24:24 +0300

    finish

---
+++ .stylelintrc.mjs
@@ -0,0 +1,10 @@
+import baseConfig from '@it-incubator/stylelint-config';
+
+const config = {
+    ...baseConfig
+};
+
+export default config;
+
+/*   pnpm i @it-incubator/prettier-config   */
+/*   pnpm add --save-dev --save-exact prettier  */
